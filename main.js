35b07818e73c57d14df09a873b2f8c559b2d4032
Musa Maluleke <86490427+Martialmeans@users.noreply.github.com>
2024-01-08 23:31:58 +0200

    update main.js

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ let screen = document.getElementById("screen");
 function appendToDisplay(x){
   screen.value += x;
 }
+
+function clearDisplay(){
+  screen.value = "";
+}
+
+function equalDisplay(){
+  screen.value = eval(screen.value);
+}
